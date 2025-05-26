a4277b961b97f34bd3cf1049eb584761f9e53568
Fernando Ismael Navarrete Ruiz <fernandonr189@outlook.com>
2025-05-25 23:03:38 -0600

    Añadidos permisos de localizacion

--- app/src/main/java/com/example/petbook/activities/MapActivity.kt
+++ app/src/main/java/com/example/petbook/activities/MapActivity.kt
@@ -1,8 +1,14 @@
 package com.example.petbook.activities
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresPermission
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,25 +18,75 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import com.example.petbook.components.AlertDialogExample
 import com.example.petbook.ui.theme.PetBookTheme
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.rememberCameraPositionState
-import com.google.maps.android.compose.rememberMarkerState
 
 class MapActivity: ComponentActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var location: MutableState<Location?> = mutableStateOf(null)
+    private var showModal = mutableStateOf(false)
 
+    @SuppressLint("MissingPermission")
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            obtainLastLocation()
+        } else {
+            showModal.value = true
+        }
+    }
+
+    private fun requestLocationPermission() : Boolean {
+        return when {
+            ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                true
+            }
+
+            else -> {
+                false
+            }
+        }
+    }
+
+    @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
+    private fun obtainLastLocation() {
+        if(!requestLocationPermission()) {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+        else {
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location : Location? ->
+                    this.location.value = location
+                }
+        }
+    }
+
+    @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        obtainLastLocation()
         setContent {
+            val currentLocation by this.location
+            val showModal by this.showModal
             PetBookTheme(darkTheme = false, dynamicColor = false) {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Surface(
@@ -40,15 +96,26 @@ class MapActivity: ComponentActivity() {
                         color = MaterialTheme.colorScheme.background
                     ) {
                         Column(
-                            modifier = Modifier.fillMaxHeight(),
-                            horizontalAlignment = Alignment.CenterHorizontally
+                            modifier = Modifier.fillMaxHeight().padding(16.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
                         ) {
                             Text(
                                 "Veterinarios en tu zona",
                                 fontSize = 32.sp,
                                 textAlign = TextAlign.Center
                             )
-                            MapPreview()
+                            currentLocation?.let { MapPreview(it) }
+                            if (showModal) {
+                                AlertDialogExample(
+                                    onDismissRequest = {
+                                        this@MapActivity.showModal.value = false
+                                        finish()
+                                    }, onConfirmation = {
+                                        this@MapActivity.showModal.value = false
+                                        obtainLastLocation()
+                                    }, "Acceso a la ubicación", "Para continuar es necesario solicitar acceso a la localización"
+                                )
+                            }
                         }
                     }
                 }
@@ -57,21 +124,15 @@ class MapActivity: ComponentActivity() {
     }
 
     @Composable
-    fun MapPreview() {
-        val singapore = LatLng(1.35, 103.87)
-        val singaporeMarkerState = rememberMarkerState(position = singapore)
+    fun MapPreview(location: Location) {
+        val latLng = LatLng(location.latitude, location.longitude)
         val cameraPositionState = rememberCameraPositionState {
-            position = CameraPosition.fromLatLngZoom(singapore, 10f)
+            position = CameraPosition.fromLatLngZoom(latLng, 10f)
         }
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState
         ) {
-            Marker(
-                state = singaporeMarkerState,
-                title = "Singapore",
-                snippet = "Marker in Singapore"
-            )
         }
     }
 }
